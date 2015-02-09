a3cb0c4f3076be0bdafdbd778898d0df2efb14fd
Igor Akulov <egor@akulovs.com>
2015-02-10 02:39:39 +0500

    before dashboard changed

--- client/ra.js
+++ client/ra.js
@@ -10,15 +10,28 @@ Meteor.startup(function() {
         var ra = document.querySelector('#ra-basic-layout');
         var lb = document.querySelector('body /deep/ ra-login-box');
 
-        Meteor.user() ? ra.isLogged = true : ra.isLogged = false
+        if (Meteor.user()) {
+            ra.isLogged = true;
+            Meteor.user().username === "admin" ? ra.isAdmin = true : ra.isAdmin = false;
+            ra.menuFabric();
+        }
+        else {
+            ra.isLogged = false 
+        };
 
         ra.addEventListener('login-box-login-try', function (e) {
             Meteor.loginWithPassword(e.detail.identifier, e.detail.password, function (err) {
-                // DEBUG console.log("login:", e, err);
-                err ? lb.shake(lb) : ra.isLogged = true;
-                /* shake login form if login incorrect */
-                /* set this if login correct */
-            })
+                if(err){
+                    lb.shake(lb)
+                }
+                else {
+                    ra.isLogged = true;
+                    if (e.detail.identifier === "admin") {
+                        ra.isAdmin = true;
+                        ra.menuFabric();
+                    }
+                }
+            });
         });
 
         ra.addEventListener('logout-action', function (e) {
@@ -27,35 +40,48 @@ Meteor.startup(function() {
 
         ra.addEventListener('crud-action', function (e) {
 
-            // e.detail = Object {action: "create", collection: "players", object: Object, elementid: "Y9NsSkA5nmDZRrHL3"}
-
-            var collection = e.detail.collection.charAt(0).toUpperCase() + e.detail.collection.slice(1);
+            // detail = Object {action: "create", collection: "players", obj: Object, elementid: "Y9NsSkA5nmDZRrHL3"}
+            var detail = e.detail;
+            var collection = detail.collection.charAt(0).toUpperCase() + detail.collection.slice(1);
             var ra = document.querySelector('#ra-basic-layout');
+            
             var result = undefined;
-            if (e.detail.collection && e.detail.collection !== "dashboard" && e.detail.object){
-                // DEBUG console.log("CRUD obj:", e.detail);
-                switch (e.detail.action) {
+            function crud (action) {
+                switch (action) {
                     case "create": {
-                        result = window[collection].insert(e.detail.object, function (){ra.menuFabric()});
-                        ra.menuFabric();
+                        result = window[collection].insert(detail.obj, function (){ra.menuFabric()});
                         break;
                     }
                     case "update": {
-                        result = window[collection].update({_id: e.detail.elementid}, {$set: e.detail.object}, {}, function (){ra.menuFabric()});
-
+                        result = window[collection].update({_id: detail.elementid}, {$set: detail.obj}, {}, function (){ra.menuFabric()});
                         break;
                     }
                     case "delete": {
-                        result = window[collection].remove(e.detail.elementid, function (){ra.menuFabric()});
-                        ra.menuFabric();
+                        result = window[collection].remove(detail.elementid, function (){ra.menuFabric()});
                         break;
                     }
                     default:
-                        console.log("ERROR: unknown crud action:", e.detail.action)
+                        console.log("ERROR: unknown crud action:", detail.action)
                 }
+            };
+            if (detail.collection && detail.collection !== "dashboard" && detail.obj){
+                // DEBUG 
+                console.log("Regular CRUD params:", detail.obj, detail.elementid);
+                crud(detail.action);
             }
             else
-                console.log("CRUD not validated:", e.type, collection);
+                if (collection === "Dashboard") {
+                    // DEBUG 
+                    console.log("Dashboard CRUD params", detail.obj, detail.elementid); return;
+/*                    var players = Players.find().fetch();
+                    for (var i in ra.players) {
+                        if (players[i].designId !== ra.players[i].designId) {
+                            console.log("DO REPLACE!!!")
+                        }
+                    } */
+                }
+                else
+                    console.log("ERROR: CRUD not validated:", e.type, detail.action, collection);
         }); // CRUD action
 
         Tracker.autorun(function() {
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,256 +1,307 @@
+Meteor.startup(function()
+{
+    /* Users fixture */
+    if (Meteor.users.find().fetch().length === 0){
+       Accounts.createUser({username: "admin", email: "admin@friendsdiner.ru", password: "admin", profile: {firstName: "Admin", lastName: "FriendsDiner"}});
+       Accounts.createUser({username: "user", email: "user@friendsdiner.ru", password: "user", profile: {firstName: "User", lastName: "FriendsDiner"}}); 
+    }
+    // 
 
-/* ---------- Config fixture ---------------------- */
+    /* ---------- Config fixture ---------------------- */
 
-if (Config.find().count() === 0) {
-    Config.insert({
-        origin: '',
-        staticOrigin: ''
-    });
-}
-/* ---------- Players fixtures ---------------------- */
+    if (Config.find().count() === 0)
+    {
+        Config.insert(
+        {
+            origin: '',
+            staticOrigin: ''
+        });
+    } /* If Config */
+    /* ---------- Players fixtures ---------------------- */
 
-if (Players.find().count() === 0) {
-    Players.insert({
-        name: 'pl1',
-        title: 'Входная галерея',
-        designId: '',
-        typeId: '720p-6x1-h',
-        ip: '10.7.0.1',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+    if (Players.find().count() === 0)
+    {
+        Players.insert(
+        {
+            name: 'pl1',
+            title: 'Входная галерея',
+            designId: '',
+            typeId: '720p-6x1-h',
+            ip: '10.7.0.1',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl2',
-        title: 'Кофейня',
-        designId: '',
-        typeId: '720p-4x1-v',
-        ip: '10.7.0.2',
-        login: 'rationale',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl2',
+            title: 'Кофейня',
+            designId: '',
+            typeId: '720p-4x1-v',
+            ip: '10.7.0.2',
+            login: 'rationale',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl3',
-        title: 'Полки кофейня',
-        designId: '',
-        typeId: '720p-5x1-h',
-        ip: '10.7.0.3',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl3',
+            title: 'Полки кофейня',
+            designId: '',
+            typeId: '720p-5x1-h',
+            ip: '10.7.0.3',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl4',
-        title: 'Полки Ф1',
-        designId: '',
-        typeId: '720p-5x1-h',
-        ip: '10.7.0.4',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl4',
+            title: 'Полки Ф1',
+            designId: '',
+            typeId: '720p-5x1-h',
+            ip: '10.7.0.4',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl5',
-        title: 'Полки бар',
-        designId: '',
-        typeId: '720p-5x1-h',
-        ip: '10.7.0.5',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl5',
+            title: 'Полки бар',
+            designId: '',
+            typeId: '720p-5x1-h',
+            ip: '10.7.0.5',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl6',
-        title: 'Бар левый',
-        designId: '',
-        typeId: '720p-3x2-h',
-        ip: '10.7.0.6',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl6',
+            title: 'Бар левый',
+            designId: '',
+            typeId: '720p-3x2-h',
+            ip: '10.7.0.6',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl7',
-        title: 'Бар правый',
-        designId: '',
-        typeId: '720p-2x2-h',
-        ip: '10.7.0.7',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl7',
+            title: 'Бар правый',
+            designId: '',
+            typeId: '720p-2x2-h',
+            ip: '10.7.0.7',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl8',
-        title: 'Бар проход',
-        designId: '',
-        typeId: '720p-4x1-h',
-        ip: '10.7.0.8',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl8',
+            title: 'Бар проход',
+            designId: '',
+            typeId: '720p-4x1-h',
+            ip: '10.7.0.8',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl9',
-        title: 'Зеркала',
-        designId: '',
-        typeId: '720p-3x1-v',
-        ip: '10.7.0.9',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
+        Players.insert(
+        {
+            name: 'pl9',
+            title: 'Зеркала',
+            designId: '',
+            typeId: '720p-3x1-v',
+            ip: '10.7.0.9',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
 
-    Players.insert({
-        name: 'pl10',
-        title: 'Витрина',
-        designId: '',
-        typeId: '720p-3x1-v',
-        ip: '10.7.0.10',
-        login: 'egor',
-        password: 'iehfkf3432'
-    });
-}
+        Players.insert(
+        {
+            name: 'pl10',
+            title: 'Витрина',
+            designId: '',
+            typeId: '720p-3x1-v',
+            ip: '10.7.0.10',
+            login: 'egor',
+            password: 'iehfkf3432'
+        });
+    } /* If Players */
 
-/* ---------- Designs fixtures ---------------------- */
+    /* ---------- Designs fixtures ---------------------- */
 
-if (Designs.find().count() === 0) {
-    Designs.insert({
-        name: 'pl1-реклама',
-        title: 'Входная группа',
-        origin: 'http://10.1.2.1/pl1',
-        content: '',
-        typeId: "720p-6x1-h"
-    });
+    if (Designs.find().count() === 0)
+    {
+        Designs.insert(
+        {
+            name: 'pl1-реклама',
+            title: 'Входная группа',
+            origin: 'http://10.1.2.1/pl1',
+            content: '',
+            typeId: "720p-6x1-h"
+        });
 
-    Designs.insert({
-        name: 'pl2',
-        title: 'Кофейня',
-        origin: 'http://10.1.2.1/pl2',
-        content: '',
-        typeId: "720p-4x1-v"
-    });
+        Designs.insert(
+        {
+            name: 'pl2',
+            title: 'Кофейня',
+            origin: 'http://10.1.2.1/pl2',
+            content: '',
+            typeId: "720p-4x1-v"
+        });
 
-    Designs.insert({
-        name: 'pl3-5',
-        title: 'Стеллажи',
-        origin: 'http://10.1.2.1/pl3',
-        content: '',
-        typeId: "720p-5x1-h"
-    });
+        Designs.insert(
+        {
+            name: 'pl3-5',
+            title: 'Стеллажи',
+            origin: 'http://10.1.2.1/pl3',
+            content: '',
+            typeId: "720p-5x1-h"
+        });
 
-    Designs.insert({
-        name: 'pl6',
-        title: 'Бар левый',
-        origin: 'http://10.1.2.1/pl6',
-        content: '',
-        typeId: "720p-3x2-h"
-    });
+        Designs.insert(
+        {
+            name: 'pl6',
+            title: 'Бар левый',
+            origin: 'http://10.1.2.1/pl6',
+            content: '',
+            typeId: "720p-3x2-h"
+        });
 
-    Designs.insert({
-        name: 'pl7',
-        title: 'Бар правый',
-        origin: 'http://10.1.2.1/pl7',
-        content: '',
-        typeId: "720p-2x2-h"
-    });
+        Designs.insert(
+        {
+            name: 'pl7',
+            title: 'Бар правый',
+            origin: 'http://10.1.2.1/pl7',
+            content: '',
+            typeId: "720p-2x2-h"
+        });
 
-    Designs.insert({
-        name: 'pl8',
-        title: 'Стеллажи 8',
-        origin: 'http://10.1.2.1/pl8',
-        content: '',
-        typeId: ""
-    });
+        Designs.insert(
+        {
+            name: 'pl8',
+            title: 'Стеллажи 8',
+            origin: 'http://10.1.2.1/pl8',
+            content: '',
+            typeId: ""
+        });
 
-    Designs.insert({
-        name: 'pl9',
-        title: 'Зеркала',
-        origin: 'http://10.1.2.1/pl9',
-        content: '',
-        typeId: "720p-4x1-h"
-    });
+        Designs.insert(
+        {
+            name: 'pl9',
+            title: 'Зеркала',
+            origin: 'http://10.1.2.1/pl9',
+            content: '',
+            typeId: "720p-4x1-h"
+        });
 
-    Designs.insert({
-        name: 'pl10',
-        title: 'Витрина',
-        origin: 'http://10.1.2.1/pl10',
-        content: '',
-        typeId: "720p-3x1-v"
-    });
-}
+        Designs.insert(
+        {
+            name: 'pl10',
+            title: 'Витрина',
+            origin: 'http://10.1.2.1/pl10',
+            content: '',
+            typeId: "720p-3x1-v"
+        });
+    } /* If Designs */
 
-/* ---------- Suites fixtures ---------------------- */
+    /* ---------- Suites fixtures ---------------------- */
 
-if (Suites.find().count() === 0) {
-    Suites.insert({
-        name: 'Футбол',
-        title: 'Футбольная',
-        pairs: [{player: 'playerId', design: 'designId'}]
-    });
-}
+    if (Suites.find().count() === 0)
+    {
+        Suites.insert(
+        {
+            name: 'Футбол',
+            title: 'Футбольная',
+            pairs: [
+            {
+                player: 'playerId',
+                design: 'designId'
+            }]
+        });
+    } /* If Suites */
 
-/* ---------- Player type fixtures ---------------------- */
+    /* ---------- Player type fixtures ---------------------- */
 
-if (PlayerTypes.find().count() === 0) {
+    if (PlayerTypes.find().count() === 0)
+    {
 
-    PlayerTypes.insert({
-        name: '720p-6x1-h',
-        title: '720p 6 горизонтальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-5x1-h',
-        title: '720p 5 горизонтальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-4x1-h',
-        title: '720p 4 горизонтальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-3x1-h',
-        title: '720p 3 горизонтальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-2x1-h',
-        title: '720p 2 горизонтальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-1x1-h',
-        title: '720p 1 горизонтальный',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-6x1-v',
-        title: '720p 6 вертикальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-5x1-v',
-        title: '720p 5 вертикальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-4x1-v',
-        title: '720p 4 вертикальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-3x1-v',
-        title: '720p 3 вертикальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-2x1-v',
-        title: '720p 2 вертикальных',
-        params: ""
-    });
-    PlayerTypes.insert({
-        name: '720p-1x1-v',
-        title: '720p 1 вертикальный',
-        params: ""
-    });
-}
+        PlayerTypes.insert(
+        {
+            name: '720p-6x1-h',
+            title: '720p 6 горизонтальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-5x1-h',
+            title: '720p 5 горизонтальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-4x1-h',
+            title: '720p 4 горизонтальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-3x1-h',
+            title: '720p 3 горизонтальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-2x1-h',
+            title: '720p 2 горизонтальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-1x1-h',
+            title: '720p 1 горизонтальный',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-6x1-v',
+            title: '720p 6 вертикальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-5x1-v',
+            title: '720p 5 вертикальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-4x1-v',
+            title: '720p 4 вертикальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-3x1-v',
+            title: '720p 3 вертикальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-2x1-v',
+            title: '720p 2 вертикальных',
+            params: ""
+        });
+        PlayerTypes.insert(
+        {
+            name: '720p-1x1-v',
+            title: '720p 1 вертикальный',
+            params: ""
+        });
+    } /* If PlayerTypes */
+}); /* Meteor startup */
+
